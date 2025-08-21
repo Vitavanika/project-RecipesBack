11de64999846a0ff8ac41ed2dbe111d1637c6b53
Kateryna Troyan <ket.tro@gmail.com>
2025-08-21 16:56:07 +0300

    Feature/get recipe by (#3)
    * getPublicRecipeById
    
    * fix package.json
    * fix: package-lock.json
    
    * fix:RecipesCollection and getById.yaml

--- src/routers/recipes.js
+++ src/routers/recipes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { getRecipeByIdController } from "../controllers/recipes.js";
+import { isValidId } from '../middlewares/isValidId.js';
+import ctrlWrapper  from '../utils/ctrlWrapper.js';
 
 const recipesRouter = Router();
+
+recipesRouter.get('/:id',isValidId, ctrlWrapper(getRecipeByIdController))
 
 export default recipesRouter;
