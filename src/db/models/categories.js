a0d0cd79ebff33cb359a8904931f833bf01ad2ce
Shade <kulakaa073@gmail.com>
2025-08-21 11:55:48 +0300

    moving models to src

--- src/db/models/categories.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const categoriesSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-});
-
-export const CategoriesCollection = model('categories', categoriesSchema);
--- src/db/models/ingredients.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const ingredientsSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-});
-
-export const IngredientsCollection = model('ingredients', ingredientsSchema);
