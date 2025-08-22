fe798c98a9b0c6e9c1c19867c4df2b79bbc30dcb
Dmytro Tsaplin <124576280+vidklyk@users.noreply.github.com>
2025-08-22 16:33:18 +1000

    Implement /api/categories endpoint and Swagger docs (#4)
    * Implement /api/categories endpoint and Swagger docs
    
    * Update dependencies (package.json & package-lock.json)
    
    * fix openapi
    
    * fix openapi add dot
    
    * Translate categories endpoint docs to English
    
    * Fix Category schema duplication and regenerate Swagger bundle

---
+++ src/routers/categories.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { getCategories } from '../controllers/categories.js';
+
+const router = Router();
+
+router.get('/', ctrlWrapper(getCategories));
+
+export default router;
