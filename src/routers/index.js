1fc7847e8a4efafde93f9d63fa877b2b285363a1
Vitavanika <vitavanika@gmail.com>
2025-08-22 09:17:22 +0200

    Revert "Implement /api/categories endpoint and Swagger docs (#4)"
    This reverts commit fe798c98a9b0c6e9c1c19867c4df2b79bbc30dcb.

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth.js';
 import recipesRouter from './recipes.js';
-import categoriesRouter from './categories.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/recipes', recipesRouter);
-router.use('/categories', categoriesRouter);
 
 export default router;
--- src/services/categories.js
+++
@@ -1,6 +0,0 @@
-import Category from '../models/category.js';
-
-export const getAllCategories = async () => {
-  const items = await Category.find().lean();
-  return items;
-};
