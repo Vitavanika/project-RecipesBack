4fd3ffbb69f5f460984b809825968a9b57a9b0b9
Lili <Liliia_Nel@proton.me>
2025-08-22 16:22:55 +0200

    clean up

--- src/services/createRecipe.js
+++ src/services/createRecipe.js
@@ -4,29 +4,25 @@ import { CategoriesCollection } from "../models/category.js";
 
 export const createRecipe = async (payload) => {
     const allIngredients = await IngredientsCollection.find({});
-    console.log(allIngredients);
+
     const ingredientsWithIds = await Promise.all(
         payload.ingredients.map(async (ing) => {
             const normalizedInput = ing.name.trim().toLowerCase();
-            console.log(normalizedInput);
-
             const ingredientDoc = allIngredients.find(
                 ingDoc => ingDoc.name.trim().toLowerCase() === normalizedInput
             );
 
             if (!ingredientDoc) throw new Error(`Ingredient "${ing.name}" not found`);
-
             return { ingredientId: ingredientDoc._id, quantity: ing.quantity };
         })
     );
 
     const allCategories = await CategoriesCollection.find();
-    console.log(allCategories);
     const normalizedCategoryName = payload.category?.trim().toLowerCase();
     const categoryDoc = allCategories.find(
         cat => cat.name.trim().toLowerCase() === normalizedCategoryName
     );
-    if (!categoryDoc) throw new Error(`Category "${payload.categoryName}" not found`);
+    if (!categoryDoc) throw new Error(`Category "${payload.category}" not found`);
 
   const recipe = await RecipesCollection.create({
     ...payload,
