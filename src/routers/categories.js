b35da009b564a0d23c918d6cefab456ae9b0d1b9
Dmytro Tsaplin <124576280+vidklyk@users.noreply.github.com>
2025-08-22 16:43:53 +1000

    Revert "Implement /api/categories endpoint and Swagger docs (#4)"
    This reverts commit fe798c98a9b0c6e9c1c19867c4df2b79bbc30dcb.

--- src/routers/categories.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ctrlWrapper from '../utils/ctrlWrapper.js';
-import { getCategories } from '../controllers/categories.js';
-
-const router = Router();
-
-router.get('/', ctrlWrapper(getCategories));
-
-export default router;
