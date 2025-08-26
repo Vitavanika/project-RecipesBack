0a5f67031cc0eb6da7137218ce99f0e2f29dcbf7
Kateryna Troyan <ket.tro@gmail.com>
2025-08-26 12:29:16 +0300

    Update recipes.js

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,6 +1,8 @@
 import { RecipesCollection } from "../models/recipe.js";
 
 export const getPublicRecipeById = async (recipeId) => {
-  const recipe = await RecipesCollection.findById(recipeId);
+  const recipe = await RecipesCollection.findById(recipeId)
+    .populate("ingredients.ingredientId", "name") 
+    .populate("categoryId", "name"); 
   return recipe;
 };
